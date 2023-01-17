6c4d2b1ec03840b1ba7dc918a627a60a55fc370d
Gomez Tomas Gonzalo <talk.gtg@gmail.com>
2023-01-17 00:54:37 -0300

    Rutas para recursos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskPriorityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('tasks', TaskController::class);
+Route::resource('priorities', TaskPriorityController::class);
